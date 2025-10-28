ddc56d19eae80d4221c3195ac33e01c96dfb7a78
HighBypass <68455891+HighBypass@users.noreply.github.com>
2025-10-27 22:16:16 -0400

    Install tailwind and put my name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-[#f1f6ff]">
+      <h1 className="text-8xl font-extrabold text-[#89adff] text-center">
+        Dowon Kim
+      </h1>
     </div>
   );
 }
